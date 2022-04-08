a5a44ca66fb38cfb58148b26258552a9d24ea0e1
kristineatease1 <kristineatease@gmail.com>
2022-04-08 15:03:01 -0700

    for task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 /**
- * main -Entry point
- *
- * Return:Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-char c;
+int n;
 
-for (c = 'a'; c <= 'z'; c++)
-putchar(c);
-putchar ('\n');
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-return(0);
-}
+if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is positive\n", n);
+
+return (0);
