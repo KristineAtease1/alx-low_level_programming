5504a97e9824c5954bc2b0d2767b498daac91eed
kristineatease1 <kristineatease@gmail.com>
2022-04-12 14:59:17 -0700

    for 10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+void pr_digit(int charcode, int fmt);
+int add(int a, int b);
+void pr_with_fmt(int charcode, int fmt);
+void print_to_98(int n);
 
 #endif
