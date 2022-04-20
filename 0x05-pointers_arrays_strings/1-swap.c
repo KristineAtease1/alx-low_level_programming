b4d4538f720268c9b2e71680feb1ccd410ca2f20
kristineatease1 <kristineatease@gmail.com>
2022-04-20 13:58:55 -0700

    for task 1 again

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,9 +2,10 @@
 #include "main.h"
 
 /**
- *swap_int - function taht swaps values of two integers
- *
- *Return 0
+ *swap_int - function taht swaps values of two integers.
+ *@a: value that will be evaluated.
+ *@b: value that will be evaluated.
+ *Return: not
  */
 
 void swap_int(int *a, int *b)
