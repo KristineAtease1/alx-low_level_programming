dee906e57397b5f6fd88fb6adc6ff3ab83846507
kristineatease1 <kristineatease@gmail.com>
2022-05-13 11:30:56 -0700

    task 0 again

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,14 +2,15 @@
 #define FUNC_POINTERS
 
 /**
- * File : function_pointers.h
- * By : Kristine Atease
+ * File: function_pointers.h
+ * Author: Kristine Atease
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-
-#endif 
+#endif
