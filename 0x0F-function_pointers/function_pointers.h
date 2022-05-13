0357e5f709c8f07039f06d0b88dc9402336b6beb
kristineatease1 <kristineatease@gmail.com>
2022-05-13 11:21:59 -0700

    task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNC_POINTERS
+#define FUNC_POINTERS
+
+/**
+ * File : function_pointers.h
+ * By : Kristine Atease
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+
+#endif 
